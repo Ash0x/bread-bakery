b8f7745cf2e3fa09ec8e84252aa0e0de021ff7aa
Ash0x <alex.galev@yahoo.com>
2021-03-13 15:23:32 -0700

    simulating menu offerings from local data set

--- src/components/views/Menu.js
+++ src/components/views/Menu.js
@@ -2,15 +2,36 @@ import React from 'react'
 
 import Item from '../Item'
 
+const DUMMY_DATA = [{
+  id: 1,
+  name: 'Sourdough',
+  weight: '1lb',
+  image: '',
+  price: '$5'
+},{
+  id: 2,
+  name: 'Whole Wheat',
+  weight: '1.5lb',
+  image: '',
+  price: '$4'
+},{
+  id: 3,
+  name: 'Whole Grain',
+  weight: '0.5lb',
+  image: '',
+  price: '$5'
+},{
+  id: 1,
+  name: 'Soda Bread',
+  weight: '1lb',
+  image: '',
+  price: '$2'
+},
+]
+
 export default function Menu(props) {
   return (
     <div style={{marginTop: '10vh'}}>
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
       <Item />
       <Item />
     </div>
