f9d696d8b8b36752b6a1d5e292109fd7620768a2
Ash0x <alex.galev@yahoo.com>
2021-03-13 17:11:32 -0700

    data added from a JSON

--- src/components/views/Menu.js
+++ src/components/views/Menu.js
@@ -2,37 +2,12 @@ import React from 'react'
 
 import Item from '../Item'
 
-const OFFERINGS = [{
-  id: 1,
-  name: 'Sourdough',
-  weight: '1lb',
-  image: '/assets/Sourdough.jpg',
-  price: '$5'
-},{
-  id: 2,
-  name: 'Whole Wheat',
-  weight: '1.5lb',
-  image: '/assets/WholeWheat.jpg',
-  price: '$4'
-},{
-  id: 3,
-  name: 'Whole Grain',
-  weight: '0.5lb',
-  image: '/assets/WholeGrain.jpg',
-  price: '$5'
-},{
-  id: 4,
-  name: 'Soda Bread',
-  weight: '1lb',
-  image: '/assets/SodaBread.jpg',
-  price: '$2'
-},
-]
+const OFFERINGS = require('../../offerings.json')
 
 export default function Menu(props) {
   return (
-    <div style={{marginTop: '10vh'}}>
-      <Item items={OFFERINGS}/>
+    <div style={{ marginTop: '10vh' }}>
+      <Item items={OFFERINGS} />
     </div>
   )
 }
