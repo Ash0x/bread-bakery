d4c4b014370fb56b544d8eb923b7d01941cf6923
Ash0x <alex.galev@yahoo.com>
2021-03-14 13:16:36 -0600

    additional data added from a JSON

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,7 +5,7 @@ import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import ButtonBase from '@material-ui/core/ButtonBase'
-
+import Button from '@material-ui/core/Button'
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -35,41 +35,37 @@ export default function Item(props) {
     <div className={classes.root}>
       {props.items.map((item) => (
         <Paper key={item.id} className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item>
-            <ButtonBase className={classes.image}>
-              <img
-                className={classes.img}
-                alt='complex'
-                src={item.image}
-              />
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction='column' spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant='subtitle1'>
-                  {item.name}
-                </Typography>
-                <Typography variant='body2' gutterBottom>
-                  {item.weight}
-                </Typography>
-                <Typography variant='body2' color='textSecondary'>
-                  {item.weight}
-                </Typography>
+          <Grid container spacing={2}>
+            <Grid item>
+              <ButtonBase className={classes.image}>
+                <img className={classes.img} alt='product_image' src={item.image} />
+              </ButtonBase>
+            </Grid>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction='column' spacing={2}>
+                <Grid item xs>
+                  <Typography gutterBottom variant='subtitle1'>
+                    {item.name}, {item.weight}
+                  </Typography>
+                  <Typography variant='body2' gutterBottom>
+                    {item.description}
+                  </Typography>
+                  <Typography variant='body2' color='textSecondary'>
+                    Contains: {item.ingredients}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color='secondary'>
+                    Buy Now
+                  </Button>
+                </Grid>
               </Grid>
               <Grid item>
-                <Typography variant='body2' style={{ cursor: 'pointer' }}>
-                  {item.price}
-                </Typography>
+                <Typography variant='subtitle1'>{item.price}</Typography>
               </Grid>
             </Grid>
-            <Grid item>
-              <Typography variant='subtitle1'>{item.price}</Typography>
-            </Grid>
           </Grid>
-        </Grid>
-      </Paper>
+        </Paper>
       ))}
     </div>
   )
