6c703503a95498d6ef4a4ff2bb6a11564f532c57
Ash0x <alex.galev@yahoo.com>
2021-03-18 13:17:04 -0600

    Created a form using the Formik package in the Order component for outputting user input data

--- src/components/views/Order.js
+++ src/components/views/Order.js
@@ -1,11 +1,104 @@
-import React from 'react'
+import * as React from 'react'
 
-export default function Order(props) {
+import Grid from '@material-ui/core/Grid'
+import { Formik, Form, Field } from 'formik'
+import { Button, CircularProgress } from '@material-ui/core'
+import { TextField } from 'formik-material-ui'
+
+interface Values {
+  email: string;
+  password: string;
+}
+
+const formStyle = {
+  padding: '0.5vh',
+}
+
+const textInputStyle = {
+  backgroundColor: 'whitesmoke',
+  margin: '0.5vh',
+}
+
+export default function Order() {
   return (
-    <div>
-      <h1>THIS IS THE FORM</h1>
-      <h2>THIS IS THE FORM</h2> <h3>THIS IS THE FORM</h3>
-      <h1>THIS IS THE FORM</h1>
-    </div>
+    <Formik
+      initialValues={{
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: '',
+      }}
+      validate={(values) => {
+        const errors: Partial<Values> = {}
+        if (!values.email) {
+          errors.email = 'Enter your email'
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address'
+        }
+        return errors
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          setSubmitting(false)
+          alert(JSON.stringify(values, null, 2))
+        }, 1000)
+      }}>
+      {({ submitForm, isSubmitting }) => (
+        <Grid container justify='center'>
+          <Form style={formStyle}>
+            <Grid item xs={12}>
+              <Field
+                style={textInputStyle}
+                component={TextField}
+                name='firstname'
+                type='firstname'
+                label='First Name'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Field
+                style={textInputStyle}
+                component={TextField}
+                name='lastname'
+                type='lastname'
+                label='Last Name'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Field
+                style={textInputStyle}
+                component={TextField}
+                name='email'
+                type='email'
+                label='Email'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Field
+                style={textInputStyle}
+                component={TextField}
+                type='password'
+                label='Password'
+                name='password'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              {isSubmitting && <CircularProgress color='secondary' />}
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant='contained'
+                color='primary'
+                disabled={isSubmitting}
+                onClick={submitForm}>
+                Submit
+              </Button>
+            </Grid>
+          </Form>
+        </Grid>
+      )}
+    </Formik>
   )
 }
